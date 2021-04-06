356f725521e68b476ad9e8b094c8783b43b431a0
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-05 21:03:13 -0500

    clean the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import image from './img/svg_autumn_card.svg';
+import SvgComponent from './Image';
 import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<img src={image} className='App-logo' alt='logo' />
+		<div>
+			<SvgComponent />
 		</div>
 	);
 }
